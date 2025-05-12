25fac3d4bb37b435e3e1ef8ad283e00c8367b0ed
OSIIOSdev <OSIIOS.dev@gmail.com>
2025-05-12 15:32:22 -0700

    1. Project created and configured

---
+++ LoginApp/GreetingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingViewController.swift
+//  LoginApp
+//
+//  Created by OSIIOSdev on 12.05.2025.
+//
+
+import UIKit
+
+final class GreetingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
